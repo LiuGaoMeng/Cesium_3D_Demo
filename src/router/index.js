2fbd487d925582ea8ad4564ec45a5c305d69d3ba
lgm123589 <lgm123589@gmail.com>
2021-07-18 18:08:34 +0800

    demo1

--- src/router/index.js
+++ src/router/index.js
@@ -7,28 +7,29 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Develop',
-    meta:'开发',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Develop.vue')
-  },
+    name: 'Map',
+    meta:'地图',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Map.vue')
+  }
+  ,
   {
-    path: '/Install',
-    name: 'Install',
-    meta:'安装',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Install.vue')
+    path: '/Point',
+    name: 'Point',
+    meta:'点实体',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Point.vue')
   },
-  {
-    path: '/Test',
-    name: 'Test',
-    meta:'测试组件',
-    component: Test
-  },
-  {
-    path: '/Calendar',
-    name: 'Calendar',
-    meta:'日历',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Calendar.vue')
-  },
+  // {
+  //   path: '/Test',
+  //   name: 'Test',
+  //   meta:'测试组件',
+  //   component: Test
+  // },
+  // {
+  //   path: '/Calendar',
+  //   name: 'Calendar',
+  //   meta:'日历',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/Calendar.vue')
+  // },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Point.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+<script type="text/javascript">
+export default {
+  name: "Point",
+  data() {
+    return {
+      tiandituTk: "ebf64362215c081f8317203220f133eb",
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      Cesium.Ion.defaultAccessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1NWI5MGUzNi1mYWI3LTQzY2QtOGI0Ni0xZWYyNTAxNGM4N2MiLCJpZCI6MTI1OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjE0NDkyNTV9.hBH0PGSnKErc_yNhIePASUkr3QPDoo0KDX9uLpNBUns";
+      var viewer = new Cesium.Viewer("cesiumContainer", {
+        // animation:false,       //动画
+        // homeButton:true,       //home键
+        // geocoder:true,         //地址编码
+        baseLayerPicker: false, //图层选择控件
+        // timeline:false,        //时间轴
+        // fullscreenButton:true, //全屏显示
+        // infoBox:true,         //点击要素之后浮窗
+        // sceneModePicker:true,  //投影方式  三维/二维
+        // navigationInstructionsInitiallyVisible:false, //导航指令
+        // navigationHelpButton:false,     //帮助信息
+        // selectionIndicator:false, // 选择
+        UrlTemplateImageryProvider:new Cesium.WebMapTileServiceImageryProvider({
+              url:
+                "http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg&tk=d6a72a78a43a2c17294b72ab26354cd6",
+              layer: "tdtImgBasicLayer",
+              style: "default",
+              format: "image/jpeg",
+              tileMatrixSetID: "GoogleMapsCompatible",
+              show: false
+              // maximumLevel: 18
+            }),
+        // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
+        //   url: "http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg&tk=d6a72a78a43a2c17294b72ab26354cd6",
+        //   layer: "tdtImgBasicLayer",
+        //   style: "default",
+        //   format: "image/jpeg",
+        //   tileMatrixSetID: "GoogleMapsCompatible",
+        //   show: false,
+        //   // maximumLevel: 18
+        // }),
+
+      });
+    },
+  },
+};
+</script>
+<style lang='less' scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
+  
